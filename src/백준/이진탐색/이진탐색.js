79e9aa8da922b70904450456ac9b19d4837ddee7
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2023-08-11 14:29:18 +0900

    백준 이진탐색 1300번,1654번,1920번, 10816번, 2110번

--- src/백준/이진탐색/이진탐색.js
+++ src/백준/이진탐색/이진탐색.js
@@ -124,9 +124,130 @@ const n6236 = () => {
     console.log(start)
 }
 
+const n1654 = () => {
+    const input = '4 11\n802\n743\n457\n539'.toString().split('\n');
+    const [N, K] = input.shift().toString().split(' ').map(Number);
+    const list = input.map(Number);
+
+    let start = 1, end = Math.max(...list)
+
+    while (start <= end) {
+        let mid = Math.floor((start+end) / 2);
+        let cnt = 0;
+        for (let i = 0; i < N; i ++) {
+            cnt += Math.floor(list[i]/mid)
+        }
+        if(cnt >= K) {
+            start = mid +1;
+        }else {
+            end = mid -1;
+        }
+    }
+    console.log(end);
+}
+
+const n1920 = () => {
+    const input = '5\n4 1 5 2 3\n5\n1 3 7 9 5'.toString().split('\n');
+    const N = Number(input.shift());
+    const A = input.shift().split(' ').map(Number).sort();
+    const M = Number(input.shift());
+    const list = input.shift().split(' ').map(Number);
+
+    const bs = (list, target, start, end) => {
+        while (start <= end) {
+            const mid = Math.floor((start + end) / 2);
+            if (target == list[mid]) return 1;
+            if (target > list[mid]) {
+                start = mid + 1;
+            } else end = mid - 1
+        }
+        return 0;
+    }
+    const answer = [];
+    for (let i = 0; i < M; i ++) {
+        answer.push(bs(A, list[i], 0, N))
+    }
+    console.log(answer.join('\n'))
+}
+
+const n10816 = () => {
+    const input = '10\n6 3 2 10 10 10 -10 -10 7 3\n8\n10 9 -5 2 3 4 5 -10'.toString().split('\n');
+    const N = Number(input.shift());
+    const A = input.shift().split(' ').map(Number).sort((a,b) => a-b);
+    const M = Number(input.shift());
+    const B = input.shift().split(' ').map(Number);
+    const answer = [];
+    const map = new Map();
+    A.forEach(x => {
+        // if(map.has(x)) map.set(x, map.get(x) + 1);
+        // else map.set(x, 1);
+        map.has(x) ? map.set(x, map.get(x) + 1) : map.set(x, 1)
+    });
+
+    B.forEach(x => answer.push(map.get(x)|0));
+    console.log(answer.join('\n'))
+}
+
+
+const n1300 = () => {
+    const input = '3\n7'.toString();
+    const [N, k] = input.split('\n').map(Number);
+
+    let start = 1;
+    let end = N * N;
+    let answer = N * N;
+
+    while (start <= end) {
+        const mid = Math.floor((start+end) / 2);
+        let cnt = 0;
+        for (let i = 1; i <= N; i ++) {
+            cnt += Math.min(Math.floor(mid/i), N)
+        }
+
+        if(cnt<k){
+            start = mid + 1;
+        } else {
+            if (answer > mid) answer = mid;
+            end = mid - 1;
+        }
+    }
+    console.log(answer)
+}
+
+const n2110 = () => {
+    const input = '5 3\n1\n2\n8\n4\n9'.toString().trim().split('\n');
+    const [N, C] = input.shift().split(' ').map(Number);
+    const list = input.map(Number).sort((a,b) => a-b);
+    let start = 1, end = Math.max(...list);
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        let cnt = 1;
+        let prev = list[0]
+        for (const cur of list) {
+            if (cur - prev < mid) continue;
+            else {
+                prev = cur;
+                cnt += 1
+            }
+        }
+
+        if(cnt < C) {
+            end = mid - 1
+        } else {
+            start = mid + 1
+        };
+    }
+    console.log(end)
+}
+
 module.exports = {
     n2805,
     n2512,
     n2343,
-    n6236
+    n1300,
+    n6236,
+    n1654,
+    n1920,
+    n10816,
+    n2110
 }
--- test/백준/이진탐색/이진탐색.test.js
+++ test/백준/이진탐색/이진탐색.test.js
@@ -15,3 +15,23 @@ test('n2343', () => {
 test('n6236',() => {
     binarySearch.n6236();
 })
+
+test('n1654',() => {
+    binarySearch.n1654();
+})
+
+test('n1920',() => {
+    binarySearch.n1920();
+})
+
+test('n10816',() => {
+    binarySearch.n10816();
+})
+
+test('n1300',() => {
+    binarySearch.n1300();
+})
+
+test('n2110',() => {
+    binarySearch.n2110();
+})
