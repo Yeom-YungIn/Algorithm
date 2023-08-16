66cd2ffac5fd3aeb192329fa28f15a3a76018d97
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2023-08-16 14:43:12 +0900

    백준 이진탐색 12015번 DFS 24479번

--- src/백준/이진탐색/이진탐색.js
+++ src/백준/이진탐색/이진탐색.js
@@ -239,7 +239,46 @@ const n2110 = () => {
     }
     console.log(end)
 }
+const n12015 = () => {
+    const [N, ...arr] = '6\n10 20 10 30 20 50'.toString().trim().split(/\s/).map(Number).sort((a,b) => a-b)
+    // const fs = require('fs');
+    // const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n").map(v => v.split(' ').map(Number))
+    // const [N] = input[0];
+    // const arr = input[1];
 
+
+    function binarySearch(left, right, target) {
+        let mid;
+        while (left < right) {
+            mid = Math.floor((left + right) / 2);
+
+            if (lis[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid
+            }
+        }
+        return right;
+    }
+
+    let lis = [];
+
+    let j = 0;
+    lis[0] = arr[0];
+    let i = 1;
+
+    while (i < N) {
+
+        if (lis[j] < arr[i]) {
+            lis[++j] = arr[i];
+        } else {
+            let idx = binarySearch(0, j, arr[i]);
+            lis[idx] = arr[i]
+        }
+        i++;
+    }
+    console.log(lis.length)
+}
 module.exports = {
     n2805,
     n2512,
@@ -249,5 +288,6 @@ module.exports = {
     n1654,
     n1920,
     n10816,
-    n2110
+    n2110,
+    n12015
 }
