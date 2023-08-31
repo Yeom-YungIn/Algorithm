86f1ea502228356820a28d74eaddb5af908105f1
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2023-08-31 16:31:02 +0900

    그래프 n1238, n13549

--- src/백준/그래프/그래프.js
+++ src/백준/그래프/그래프.js
@@ -45,5 +45,87 @@ module.exports.graph = {
         }
         for (let i = 1; i <= N; i++) !visited[i] && dfs(i)
         console.log(answer.reverse().join(' '))
+    },
+
+    n1238: () => {
+        const input = '4 8 2\n1 2 4\n1 3 2\n1 4 7\n2 1 1\n2 3 5\n3 1 2\n3 4 4\n4 2 3'.toString().trim().split('\n');
+        const [n, m, x] = input.shift().split(' ').map(Number);
+        const list = input.map(x => x.trim().split(' ').map(Number));
+
+        const allDistances = {};
+
+        const dijkstra = (n, start, graph) => {
+            const distance = Array.from({ length: n + 1 }, () => Infinity);
+            const q = [];
+            distance[start] = 0;
+            q.push([start, 0]);
+
+            while (q.length>0) {
+                const [curr, weight] = q.shift();
+                for (const [dest, cost] of graph[curr]) {
+                    if (distance[dest] > weight + cost) {
+                        distance[dest] = weight + cost;
+                        q.push([dest,distance[dest]]);
+                    }
+                }
+            }
+            allDistances[start] = distance;
+        };
+
+        const graph = {};
+        let answer = 0;
+        for (let i = 1; i <= n; i++) {
+            graph[i] = [];
+        }
+        console.log(graph)
+        list.forEach(([from, to, val]) => graph[from].push([to, val]));
+
+        for (let start = 1; start <= n; start++) {
+            dijkstra(n, start, graph);
+        }
+
+        console.log(allDistances)
+        for (let start = 1; start <= n; start++) {
+            answer = Math.max(answer, allDistances[start][x] + allDistances[x][start]);
+        }
+        console.log(answer);
+    },
+
+    n13549 : () => {
+        let [x, k] = '5 17'.toString().trim().split(' ').map(Number);
+
+        function bfs(N, K) {
+            const MAX = 100000; // 최대 위치
+            const visited = new Array(MAX + 1).fill(false);
+            const q = [];
+
+            q.push([N, 0]);
+            visited[N] = true;
+
+            while (q.length > 0) {
+                const [cur, time] = q.shift();
+
+                if (cur === K) {
+                    return time;
+                }
+
+                if (cur * 2 <= MAX && !visited[cur * 2]) {
+                    q.push([cur * 2, time]);
+                    visited[cur * 2] = true;
+                }
+
+                if (cur - 1 >= 0 && !visited[cur - 1]) {
+                    q.push([cur - 1, time + 1]);
+                    visited[cur - 1] = true;
+                }
+
+                if (cur + 1 <= MAX && !visited[cur + 1]) {
+                    q.push([cur + 1, time + 1]);
+                    visited[cur + 1] = true;
+                }
+            }
+        }
+
+        console.log(bfs(x,k))
     }
 }
