c3920ca1b4502815c06bd42d1c9035990ace695c
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2023-12-03 21:38:37 +0900

    백준 11724번

--- src/백준/DFS,BFS 추천문제/연결 요소의 개수.js
+++ src/백준/DFS,BFS 추천문제/연결 요소의 개수.js
@@ -1,35 +1,36 @@
 
 module.exports.n11724 = {
-    n11724: () => {
-        // const fs = `6 5\n1 2\n2 5\n5 1\n3 4\n4 6`
-        // const input = fs.toString().split('\n')
-        const fs = require("fs")
-        const input = fs.readdirSync('/dev/stdin').toString().split('\n')
-        const [N, M] = input.shift().split(' ').map(x => Number(x))
+    solve() {
+        // const fs = require('fs')
+        const fs = '6 5\n1 2\n2 5\n5 1\n3 4\n4 6'.toString().trim();
+        const [[N, M], ...input] = fs.split('\n').map(x => x.split(' ').map(Number));
+        console.log(N, M, input);
 
-        let cc = [], cnt = 0;
+        const visited = Array(N + 1).fill(false);
+        const graph = Array.from({length: N + 1}, () => []);
 
-        for(let i = 1; i <= N + 1; i++) cc[i] = []
-        for(const arr of input) {
-            const [a, b] = arr.split(' ').map(x => Number(x))
-            cc[a].push(b)
-            cc[b].push(a)
+        input.forEach(([a, b]) => {
+            graph[a].push(b);
+            graph[b].push(a);
+        })
+
+        let result = 0;
+        const dfs = (n) => {
+            visited[n] = true;
+            graph[n].forEach(x => {
+                if (!visited[x]) {
+                    dfs(x);
+                }
+            })
         }
-        let visited = Array(N+1).fill(0)
-        const dfs = (i) => {
-            const val = cc[i]
-            for(let j = 0; j < val.length; j ++) {
-                if(visited[val[j]] === 1) continue;
-                visited[val[j]] = 1;
-                dfs(cc[i][j])
+
+        for (let i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                result++;
             }
         }
-        for (let i = 1; i < N;  i++) {
-            if(visited[i] === 1) continue;
-            cnt ++
-            dfs(i)
-        }
 
-        return cnt
-    }
+        console.log(result);
+    },
 }
