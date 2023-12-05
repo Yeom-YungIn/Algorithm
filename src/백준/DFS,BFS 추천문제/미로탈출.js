522a1151c1dd21510763d335008d3683e1ab1a68
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2023-12-05 14:32:21 +0900

    백준 2178번 재풀이

--- src/백준/DFS,BFS 추천문제/미로탈출.js
+++ src/백준/DFS,BFS 추천문제/미로탈출.js
@@ -1,42 +1,31 @@
-const fs = require("fs");
-
 /**
  * https://www.acmicpc.net/problem/2178
  */
 module.exports.maze = {
     n2178: () => {
-        // const fs = require ('fs')
-        // const input = fs.readFileSync('/dev/stdin').toString().split('\n')
-        const fs = '4 6\n101111\n001010\n101011\n111011'
-        const input = fs.toString().split('\n')
-        const [N,M] = input[0].split(' ').map(Number)
-        const maze = input.map((row) => row.split('').map(Number))
-        maze.shift()
-        console.log(maze[0][0])
-        let visited = Array.from({length: N}, () => Array(M).fill(0))
-        visited[0][0] = 1
-        console.log(visited)
-        const dx = [-1,1,0,0];
-        const dy = [0,0,-1,1];
-        let queue = []
-        let x = 0, y=0;
-        queue.push([x,y])
+        // const fs = '7 7\n1011111\n1110001\n1000001\n1000001\n1000001\n1000001\n1111111';
+        const fs = require('fs');
+        const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+        const [n, m] = input.shift().split(' ').map(Number);
+        const map = input.map(x => x.toString().split('').map(Number));
+        const visited = Array.from({length: n}, () => Array(m).fill(false));
 
-        while (queue.length) {
-            const temp = queue.shift();
-            x = temp[0]
-            y = temp[1]
-            for(let i = 0; i < 4; i ++) {
-                const nx = x + dx[i]
-                const ny = y + dy[i]
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if(maze[nx][ny] && !visited[nx][ny]) {
-                    visited[nx][ny] = visited[x][y] + 1
-                    queue.push([nx,ny])
+        const queue = [[0,0]];
+        visited[0][0] = true;
+        const move = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+        while (queue.length > 0) {
+            const [x, y] = queue.shift();
+            for (let i = 0; i <= 3; i ++) {
+                const [nx, ny] = [x + move[i][0], y + move[i][1]];
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if (!visited[nx][ny] && map[nx][ny]) {
+                    visited[nx][ny] = true;
+                    map[nx][ny] = map[x][y] + 1;
+                    queue.push([nx, ny]);
                 }
+
             }
         }
-        console.log(visited)
-        console.log(visited[N-1][M-1])
+        console.log(map[n-1][m-1]);
     }
 }
