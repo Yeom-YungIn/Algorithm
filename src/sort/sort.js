64ee9dcca2537c933f1bed35e89ccdc59b537e1f
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-02-21 17:57:48 +0900

    bubbleSort

--- src/sort/sort.js
+++ src/sort/sort.js
@@ -1,4 +1,22 @@
 export class Sort {
+
+    bubbleSort(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            let swap;
+            for (let j = i + 1; j < arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    swap = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = swap;
+                }
+            }
+        }
+        return arr;
+    }
+
+    /***
+     * 기준 값을 선정하여 해당 값보다 작은 데이터와 큰 데이터로 분류하는 것을 반복해 정렬
+     */
     quickSort(arr) {
         if (arr.length <= 1) return arr;
         const pivot = arr[0];
